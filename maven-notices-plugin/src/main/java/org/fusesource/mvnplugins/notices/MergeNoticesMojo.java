26157fc9e281385f129b1a9833e7092f84aa49a2
Jonathan Anstey <janstey@gmail.com>
2011-09-12 15:39:07 -0230

    Add ability to merge multiple repos and config organization

--- maven-notices-plugin/src/main/java/org/fusesource/mvnplugins/notices/MergeNoticesMojo.java
+++ maven-notices-plugin/src/main/java/org/fusesource/mvnplugins/notices/MergeNoticesMojo.java
@@ -27,16 +27,21 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Properties;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.maven.model.Build;
 import org.apache.maven.model.Dependency;
 import org.apache.maven.model.Model;
+import org.apache.maven.model.Organization;
 import org.apache.maven.model.Plugin;
 import org.apache.maven.model.PluginExecution;
+import org.apache.maven.model.io.DefaultModelReader;
 import org.apache.maven.model.io.DefaultModelWriter;
+import org.apache.maven.model.io.ModelReader;
 import org.apache.maven.model.io.ModelWriter;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -70,13 +75,28 @@ public class MergeNoticesMojo extends AbstractMojo {
      * @parameter
      * @required
      */
-    private File repository;
+    private String repositories;
 
     /**
      * @parameter default-value="My Project"
      * @required
      */    
     private String projectName;
+    
+    /**
+     * @parameter default-value=""
+     */    
+    private String preamble;
+
+    /**
+     * @parameter default-value="The Apache Software Foundation"
+     */    
+    private String organizationName;
+
+    /**
+     * @parameter default-value="http://www.apache.org/"
+     */    
+    private String organizationURL;   
     
     public void execute() throws MojoExecutionException, MojoFailureException {
         try {
@@ -85,10 +105,9 @@ public class MergeNoticesMojo extends AbstractMojo {
             model.setProfiles(null);
             model.setBuild(null);
             model.setArtifactId(model.getArtifactId() + "-dependencies");
-
             String projectVersion = project.getVersion();
             
-            model.setDependencies(loadDependenciesFromRepo());
+            model.setDependencies(loadDependenciesFromRepos());
 
             Build build = new Build();
             build.addPlugin(createShadePlugin());
@@ -119,27 +138,28 @@ public class MergeNoticesMojo extends AbstractMojo {
 
     }
 
+    private List<Dependency> loadDependenciesFromRepos() {
+        if (repositories.contains(",")) {            
+            List<Dependency> deps = new ArrayList<Dependency>();
+            for (String repo : repositories.split(",")) {
+                deps.addAll(loadDependenciesFromRepo(new File(repo)));
+            }
+            return deps;
+        } else {
+            return loadDependenciesFromRepo(new File(repositories));
+        }
+    }
+
     @SuppressWarnings("unchecked")
-    private List<Dependency> loadDependenciesFromRepo() {
-        System.out.println(repository);
-        Iterator<File> jars = FileUtils.listFiles(repository, new String[] { "jar" }, true).iterator();
+    private List<Dependency> loadDependenciesFromRepo(File repo) {
+        Iterator<File> jars = FileUtils.listFiles(repo, new String[] { "jar" }, true).iterator();
         List<Dependency> deps = new ArrayList<Dependency>();
         while (jars.hasNext()) {
             File jar = jars.next();
-            String version = jar.getParentFile().getName();
-
-            String artifactId = jar.getParentFile().getParentFile().getName();
-
-            String groupIdSep = StringUtils.replace(jar.getParentFile().getParentFile().getParentFile().getPath(), repository.getPath(), "");
-            String groupId = groupIdSep.replace(File.separatorChar, '.');
-            groupId = groupId.startsWith(".") ? groupId.substring(1) : groupId;
-            groupId = groupId.endsWith(".") ? groupId.substring(0, groupId.length() - 1) : groupId;
-
-            Dependency dep = new Dependency();
-            dep.setArtifactId(artifactId);
-            dep.setGroupId(groupId);
-            dep.setVersion(version);
-            deps.add(dep);
+            Dependency dependencyFromJar = dependencyFromJar(jar, repo);
+            if (dependencyFromJar != null) {
+                deps.add(dependencyFromJar);
+            }
         }
         return deps;
     }
@@ -165,7 +185,7 @@ public class MergeNoticesMojo extends AbstractMojo {
                     dest.close();
                     break;
                 }
-            }
+            }                     
             zis.close();
         } catch (Exception e) {
             e.printStackTrace();
@@ -217,13 +237,84 @@ public class MergeNoticesMojo extends AbstractMojo {
         configuration.addChild(transformers);
         Xpp3Dom transformer = new Xpp3Dom("transformer");
         transformer.setAttribute("implementation", "org.apache.maven.plugins.shade.resource.ApacheNoticeResourceTransformer");
+
         Xpp3Dom projectNameElement = new Xpp3Dom("projectName");
         projectNameElement.setValue(projectName);
         transformer.addChild(projectNameElement);
+        
+        Xpp3Dom organizationNameElement = new Xpp3Dom("organizationName");
+        organizationNameElement.setValue(organizationName);
+        transformer.addChild(organizationNameElement);
+        
+        Xpp3Dom organizationURLElement = new Xpp3Dom("organizationURL");
+        organizationURLElement.setValue(organizationURL);
+        transformer.addChild(organizationURLElement);
+        
+        Xpp3Dom preamble1Element = new Xpp3Dom("preamble1");
+        preamble1Element.setValue(preamble);
+        transformer.addChild(preamble1Element);        
+        
         transformers.addChild(transformer);
 
         pluginExecution.setConfiguration(configuration);
         shade.addExecution(pluginExecution);
         return shade;
+    }
+
+    private Model getPom(File jar) {
+        try {
+            ZipFile zip = new ZipFile(jar);
+            FileInputStream fis = new FileInputStream(jar);
+            ZipInputStream zis = new ZipInputStream(new BufferedInputStream(fis));
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                if (entry.getName().startsWith("META-INF") && entry.getName().endsWith("pom.xml")) {
+                    ModelReader reader = new DefaultModelReader();
+                    return reader.read(zip.getInputStream(entry), null);
+                }
+            }
+            zis.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private Properties getPomProperties(File jar) {
+        try {
+            ZipFile zip = new ZipFile(jar);
+            FileInputStream fis = new FileInputStream(jar);
+            ZipInputStream zis = new ZipInputStream(new BufferedInputStream(fis));
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                if (entry.getName().startsWith("META-INF") && entry.getName().endsWith("pom.properties")) {
+                    Properties props = new Properties();
+                    props.load(zip.getInputStream(entry));
+                    return props;
+                }
+            }
+            zis.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    private Dependency dependencyFromJar(File jar, File repo) {
+        Model pom = getPom(jar);
+        Properties props = getPomProperties(jar);
+        Dependency dep = new Dependency();
+        if (props != null) {            
+            dep.setArtifactId(props.getProperty("artifactId"));
+            dep.setGroupId(props.getProperty("groupId"));
+            dep.setVersion(props.getProperty("version"));
+        } else if (pom != null) {            
+            dep.setArtifactId(pom.getArtifactId());
+            dep.setGroupId(pom.getGroupId());
+            dep.setVersion(pom.getVersion());
+        } else { 
+            return null;
+        }       
+        return dep;
     }
 }
